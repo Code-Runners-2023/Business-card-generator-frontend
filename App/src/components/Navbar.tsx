e6d8c6dac47c4b300cbc9cd8e6068c32eb794890
karuBeL <burak.bali420@gmail.com>
2023-07-06 16:32:35 +0300

    Fixed previous commit

--- App/src/components/Navbar.tsx
+++ App/src/components/Navbar.tsx
@@ -9,12 +9,9 @@ export const Navbar = ({ isLoggedIn }: { isLoggedIn: boolean }) => {
             {isLoggedIn ? null : <Link to="/login">Log In</Link>}
             {isLoggedIn ? null : <Link to="/signup">Sign Up</Link>}
             <Link to="/generate">Generate a card</Link>
-<<<<<<< HEAD
             {isLoggedIn ? <Link to="/profile">Profile</Link> : null}
-=======
             {isLoggedIn ? <Link to="/settings">Settings</Link> : null}
             {isLoggedIn ? <Link to="/preview">Card Preview</Link> : null} 
->>>>>>> 2dee1d321957b0831073b68997c3446c3887cd3e
         </nav>
     )
 }
